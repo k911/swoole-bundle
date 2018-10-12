e4153dda881c9299793c9b60fba211cf13e92eae
k911 <konradobal@gmail.com>
2018-10-12 20:45:33 +0200

    fix(io): Properly close stdout/stderr
    Detached process was running in background with not closed stream to console stdout/stderr, and for
    example monolog still printed logs to console.

--- src/Bridge/Symfony/Bundle/Command/ServerStartCommand.php
+++ src/Bridge/Symfony/Bundle/Command/ServerStartCommand.php
@@ -6,42 +6,17 @@ namespace K911\Swoole\Bridge\Symfony\Bundle\Command;
 
 use K911\Swoole\Server\HttpServer;
 use K911\Swoole\Server\HttpServerConfiguration;
-use K911\Swoole\Server\HttpServerFactory;
-use K911\Swoole\Server\Runtime\BootManager;
-use Monolog\Logger;
-use Psr\Log\LoggerInterface;
 use RuntimeException;
-use Symfony\Bridge\Monolog\Handler\ConsoleHandler;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\StreamOutput;
 use Symfony\Component\Console\Style\OutputStyle;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use function K911\Swoole\get_object_property;
-use function K911\Swoole\replace_object_property;
 
 final class ServerStartCommand extends AbstractServerStartCommand
 {
-    /**
-     * @var null|LoggerInterface
-     */
-    private $logger;
-
-    public function __construct(
-        HttpServer $server,
-        HttpServerFactory $serverFactory,
-        HttpServerConfiguration $serverConfiguration,
-        ParameterBagInterface $parameterBag,
-        BootManager $bootManager,
-        ?LoggerInterface $logger = null
-    )
-    {
-        parent::__construct($server, $serverFactory, $serverConfiguration, $parameterBag, $bootManager);
-        $this->logger = $logger;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -49,7 +24,7 @@ final class ServerStartCommand extends AbstractServerStartCommand
     {
         $this->setName('swoole:server:start')
             ->setDescription('Runs a local Swoole HTTP server in background.')
-            ->addOption('pid_file', null, InputOption::VALUE_REQUIRED, 'Pid file', $this->parameterBag->get('kernel.project_dir') . '/var/swoole.pid');
+            ->addOption('pid_file', null, InputOption::VALUE_REQUIRED, 'Pid file', $this->parameterBag->get('kernel.project_dir').'/var/swoole.pid');
 
         parent::configure();
     }
@@ -75,58 +50,32 @@ final class ServerStartCommand extends AbstractServerStartCommand
             throw new RuntimeException(\sprintf('Could not create pid file "%s".', $serverConfiguration->getPid()));
         }
 
-        /** @var ConsoleOutput $consoleOutput */
-        $consoleOutput = &get_object_property($io, 'output', OutputStyle::class);
-
-        /** @var resource $stream */
-        $stream = &get_object_property($consoleOutput, 'stream', StreamOutput::class);
-
-        \fclose($stream);
-
-        if ($this->logger instanceof Logger) {
-            $this->removeConsoleHandlerLogger($this->logger);
-        }
-
-        unset($this->logger);
-
-//        $this->forceCloseAllStdIoStreams();
-
-//        $this->getApplication()->get()
-
-//        dd(get_resources('stream'));
+        $this->closeSymfonyStyle($io);
 
         $server->start();
     }
 
-    private function removeConsoleHandlerLogger(Logger $logger): void
+    private function closeSymfonyStyle(SymfonyStyle $io): void
     {
-        $handlers = [];
-        foreach ($logger->getHandlers() as $handler) {
-            if ($handler instanceof ConsoleHandler) {
-                /** @var StreamOutput $streamOutput */
-                $streamOutput = &get_object_property($handler, 'output', ConsoleHandler::class);
+        /** @var ConsoleOutput $consoleOutput */
+        $consoleOutput = get_object_property($io, 'output', OutputStyle::class);
 
-                /** @var resource $stream */
-                $stream = &get_object_property($streamOutput, 'stream', StreamOutput::class);
-                \fclose($stream);
-
-                $stream = tmpfile();
-                replace_object_property($streamOutput, 'stream', $stream, StreamOutput::class);
-            } else {
-                $handlers[] = $handler;
-            }
-        }
-        $logger->setHandlers($handlers);
+        $this->closeConsoleOutput($consoleOutput);
     }
 
-    private function forceCloseAllStdIoStreams(): void
+    /**
+     * Prevents usage of php://stdout or php://stderr while running in background.
+     *
+     * @param ConsoleOutput $output
+     */
+    private function closeConsoleOutput(ConsoleOutput $output): void
     {
-        dump('d');
-        foreach (\get_resources('stream') as $stream) {
-            if ('STDIO' === \stream_get_meta_data($stream)['stream_type']) {
-                \fclose($stream);
-            }
-        }
-        dump('d');
+        \fclose($output->getStream());
+
+        /** @var StreamOutput $streamOutput */
+        $streamOutput = $output->getErrorOutput();
+
+        $streamOutput->setVerbosity(PHP_INT_MIN);
+        \fclose($streamOutput->getStream());
     }
 }
