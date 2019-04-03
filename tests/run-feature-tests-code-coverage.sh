1f1de2a9a34beb7203b69c85f35c44db43749a6c
k911 <konradobal@gmail.com>
2019-04-03 22:30:19 +0200

    ci(travis): Switch "travis_retry" to custom bash script

--- tests/run-feature-tests-code-coverage.sh
+++ tests/run-feature-tests-code-coverage.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
 EXIT_CODE=0
+MAX_TRIES=3
 
-i=0
+TEST_NO=0
 for f in ./tests/Feature/*.php; do
-    ((i++))
-    echo "[Test $i] $f";
-    vendor/bin/phpunit "$f" --coverage-php "cov/feature-tests-$i.cov" --colors=always || EXIT_CODE=1;
-    # Make sure server is killed for next test
-    PID=$(lsof -i :9999 | grep php | awk '{print $2}')
-    if [[ "" != "$PID" ]]; then
-        kill -9 ${PID}
-        EXIT_CODE=1
+    ((TEST_NO++))
+    echo "[Test $TEST_NO] $f";
+
+    TEST_EXIT_CODE=0
+    for ((TRY_NO=1; TRY_NO <= $MAX_TRIES; TRY_NO++)); do
+        echo "[Test $TEST_NO] Try $TRY_NO of $MAX_TRIES";
+
+        vendor/bin/phpunit "$f" --coverage-php "cov/feature-tests-$TEST_NO.cov" --colors=always
+        TEST_EXIT_CODE=$?
+
+        # Make sure server is killed for next test
+        PID=$(lsof -i :9999 | grep php | awk '{print $2}')
+        if [[ "" != "$PID" ]]; then
+            kill -9 ${PID} || true
+            sleep 1;
+        fi
+
+        if [[ "$TEST_EXIT_CODE" = "0" ]]; then
+            break;
+        fi
+        sleep 1;
+    done
+
+    if [[ "$TEST_EXIT_CODE" != "0" ]]; then
+        EXIT_CODE=1;
     fi
-    sleep 1;
+
 done
 
 exit ${EXIT_CODE}
