bfb6d4ca9be138eca3a1b12191ccacd0e6bd2548
k911 <konradobal@gmail.com>
2019-10-19 09:23:02 +0200

    chore(commitlint): Update configuration options to less restrictive
    - Check latest 20 commits instead of single one in pull request

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,9 @@
+// https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'subject-case': [
-            2,
-			'never',
-			['start-case', 'pascal-case', 'upper-case']
-        ]
+        'header-max-length': [1, 'always', 72],
+        'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
+        'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']]
     }
 };
