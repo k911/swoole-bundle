0ee89182073edfa46cefaea2d58731130d3a7252
k911 <konradobal@gmail.com>
2019-10-27 19:26:52 +0100

    fix(http-client): Make HttpClient serializable
    When any feature test failed with constructed client, PHPUnit tried to serialize `HttpClient` and since it has been failing to serialize `Swoole\Http\Client` dependency it threw exception again, so we did not have a clue about real issue.

---
+++ tests/Unit/Client/HttpClientTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace K911\Swoole\Tests\Unit\Client;
+
+use K911\Swoole\Client\HttpClient;
+use PHPUnit\Framework\TestCase;
+
+final class HttpClientTest extends TestCase
+{
+    public function testThatClientSerializesProperly(): void
+    {
+        $host = 'fake';
+        $port = 8888;
+        $ssl = false;
+        $options = ['testing' => 1];
+
+        $client = HttpClient::fromDomain($host, $port, $ssl, $options);
+
+        $expected = [
+            'host' => $host,
+            'port' => $port,
+            'ssl' => $ssl,
+            'options' => $options,
+        ];
+
+        $this->assertSame(\json_encode($expected, \JSON_THROW_ON_ERROR), $client->serialize());
+
+        $serializedClient = \serialize($client);
+        $unserializedClient = \unserialize($serializedClient, ['allowed_classes' => [HttpClient::class]]);
+
+        $this->assertInstanceOf(HttpClient::class, $unserializedClient);
+    }
+}
