fe1af90f5a3c004391ab1d3b35a78d71c2797725
k911 <konradobal@gmail.com>
2020-06-22 21:52:46 +0200

    chore(composer): upgrade deps

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [1, 'always', 72],
+        'body-max-line-length': [1, 'always', 500],
+        'footer-max-line-length': [1, 'always', 200],
+        'header-max-length': [1, 'always', 100],
         'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
         'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']]
     }
