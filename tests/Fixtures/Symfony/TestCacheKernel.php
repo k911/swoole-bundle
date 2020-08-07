ef0857c5961e613c0376511ae12190f5a15becae
Alexandr Zolotukhin <supersmile2009@gmail.com>
2020-08-07 17:10:59 +0300

    test(session-storage): test if session storage works with CacheKernel

---
+++ tests/Fixtures/Symfony/TestCacheKernel.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace K911\Swoole\Tests\Fixtures\Symfony;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class TestCacheKernel extends HttpCache
+{
+}
